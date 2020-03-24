ae41d6cf64df78c45dee53f9e44efb5b618548b7
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-03-24 20:05:31 +0530

    Add files via upload

---
+++ Wallpaper Changer using Python/test.py
@@ -0,0 +1,33 @@
+import struct
+import ctypes
+
+
+SPI_SETDESKWALLPAPER = 20
+WALLPAPER_PATH = " C:\\Users\\arbaz\\Downloads\\projects\\wallpapers-script-master\\wallpaper.jpg"
+
+
+def is_64_windows():
+    """Find out how many bits is OS. """
+    return struct.calcsize('P') * 8 == 64
+
+
+def get_sys_parameters_info():
+    """Based on if this is 32bit or 64bit returns correct version of SystemParametersInfo function. """
+    return ctypes.windll.user32.SystemParametersInfoW if is_64_windows() \
+        else ctypes.windll.user32.SystemParametersInfoA
+
+
+def change_wallpaper():
+    sys_parameters_info = get_sys_parameters_info()
+    r = sys_parameters_info(SPI_SETDESKWALLPAPER, 0, WALLPAPER_PATH, 3)
+
+    # When the SPI_SETDESKWALLPAPER flag is used,
+    # SystemParametersInfo returns TRUE
+    # unless there is an error (like when the specified file doesn't exist).
+    if not r:
+        print(ctypes.WinError())
+
+
+change_wallpaper()
+ 
+    #'C:\\Users\\Public\\Pictures\\abc.jpg'
