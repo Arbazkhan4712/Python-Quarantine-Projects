51fb9f42ba65e7c20e6ff9996e9040f1d2dbebd9
sairoyal007 <62612632+sairoyal007@users.noreply.github.com>
2020-03-25 19:49:21 +0530

    YouTube downloader.py
    YouTube downloader with GUI

--- High Qulity YouTube Video Downloader/YTD.py
+++ High Qulity YouTube Video Downloader/YTD.py
@@ -1,7 +1,23 @@
-import  youtube_dl
+from tkinter import *
+import youtube_dl
+root=Tk()
+
+root.title('Youtube downloader')
+root.geometry("370x23")
+ydl_opts = {}
+def download():
+    link_of_the_video = text_box.get()
+    zxt = link_of_the_video.strip()
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([zxt])
 
 
-arg = sys.argv[1]
-ytd={}
-with youtube_dl.YoutubeDL(ytd)as yt:
-    yt.download([arg])
+text_box=Entry(root,width=50)
+text_box.insert(string="paste link here...",index=1)
+button=Button(root,text='Download',command=download)
+text_box.grid(row=0,column=0)
+button.grid(row=0,column=1)
+
+
+
+root.mainloop()
