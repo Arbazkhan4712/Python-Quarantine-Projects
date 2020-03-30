ee0fd889e178f1c09c1bd4bdcfd044e628360410
Rohan kumar <48005711+alexakali@users.noreply.github.com>
2020-03-31 02:04:01 +0530

    Update cam.py
    changing the color from gray to BRG on cam,py

--- Face Recognition Attendence/cam.py
+++ Face Recognition Attendence/cam.py
@@ -8,9 +8,9 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    color = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',color)
     #cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
