df59f976dc06eea7461f53acfd06ab5be78a86a9
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-05-06 18:13:24 +0530

    Update Main.py

--- Raspberry-Pi-Sonoff/Main.py
+++ Raspberry-Pi-Sonoff/Main.py
@@ -1,12 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from gpiozero import LED
 from time import sleep
+led = LED(2)
 
-a = int(input("Enter Time"))
-a = a*60
-t = 1
-while True:
-    print(t)
-    t = t+1
-    if t == a:
-        print("Done")
-        exit()
-    sleep(1)
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    if led.value == 1:
+        status = 'ON'
+    else:
+        status = 'OFF'
+    return render_template('home.html', status=status)
+
+
+@app.route("/on")
+
+def on():
+    led.on()
+    return "LED on"
+
+@app.route("/off")
+
+def off():
+    led.off()
+    return "LED off"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000) 
+
